d4bc0f7328eb1f979ba5e87b5a0e5e3d5834f505
hovaroyan <hovaroyan@gmail.com>
2021-07-17 21:18:37 +0400

    added button component and design fix

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+@Input() text: string = "";
+@Input() color: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
